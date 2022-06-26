b853fa678664da40192fb75064b589cf2f444f2b
Prawoto Hadi <pawoto.hadi@gmail.com>
2022-06-26 22:29:36 +0700

    Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css'
 
 const Header = () => {
     return (
-        <></>
+        <header>
+            <h1 className="title">Call A Friend</h1>
+            <h3 className="subtitle">Your Friendly Contact App</h3>
+        </header>
     )
 }
 
